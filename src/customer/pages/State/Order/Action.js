b8bffc4fdf60482300031eda845d95236c6005df
vrushaliTambat <vrushalitambat32@gmail.com>
2024-03-29 11:23:33 -0400

    cart & order reducer

---
+++ src/customer/pages/State/Order/Action.js
@@ -0,0 +1,44 @@
+import { api } from "../../../config/api";
+import { createOrderFailure, createOrderRequest, createOrderSuccess, getUsersOrdersFailure, getUsersOrdersRequest, getUsersOrdersSuccess } from "./ActionCreators";
+import { CREATE_ORDER_FAILURE, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, GET_USERS_NOTIFICATION_FAILURE, GET_USERS_NOTIFICATION_SUCCESS, GET_USERS_ORDERS_FAILURE, GET_USERS_ORDERS_REQUEST, GET_USERS_ORDERS_SUCCESS } from "./ActionType";
+
+
+export const createOrder = (reqData) => {
+  return async (dispatch) => {
+    dispatch({type:CREATE_ORDER_REQUEST});
+    try {
+      const {data} = await api.post('/api/order', reqData.order,{
+        headers: {
+            Authorization: `Bearer ${reqData.jwt}`,
+          },
+      });
+      // if(data.payment_url){
+      //   window.location.href=data.payment_url;
+      // }
+      console.log("created order data",data)
+      dispatch({type:CREATE_ORDER_SUCCESS,payload:data});
+    } catch (error) {
+      console.log("error ",error)
+      dispatch({type:CREATE_ORDER_FAILURE,payload:error});
+    }
+  };
+};
+
+
+export const getUsersOrders = (jwt) => {
+  return async (dispatch) => {
+    dispatch({type:GET_USERS_ORDERS_REQUEST});
+    try {
+      const {data} = await api.get(`/api/order/user`,{
+        headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+      });
+      console.log("users order ",data)
+      dispatch({type:GET_USERS_ORDERS_SUCCESS,payload:data});
+    } catch (error) {
+      dispatch({type:GET_USERS_ORDERS_FAILURE,payload:error});
+    }
+  };
+};
+
--- src/customer/pages/State/store.js
+++ src/customer/pages/State/store.js
@@ -3,11 +3,15 @@ import { authReducer } from "./Authentication/Reducer";
 import { thunk } from "redux-thunk";
 import restaurantReducer from "./Restaurant/Reducer";
 import menuItemReducer from "./Menu/Reducer";
+import cartReducer from "./Cart/Reducer";
+import { orderReducer } from "./Order/Reducer";
 
 const rootReducer=combineReducers({
     auth:authReducer,
     restaurant:restaurantReducer,
-    menu:menuItemReducer
+    menu:menuItemReducer,
+    cart:cartReducer,
+    order:orderReducer
 })
 
 export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
