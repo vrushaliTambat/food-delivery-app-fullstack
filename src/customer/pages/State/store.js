e3de361668414655b8b162e86645ff37a076f424
vrushaliTambat <vrushalitambat32@gmail.com>
2024-03-28 23:05:31 -0400

    menu action & reducer

--- src/customer/pages/State/store.js
+++ src/customer/pages/State/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { authReducer } from "./Authentication/Reducer";
 import { thunk } from "redux-thunk";
 import restaurantReducer from "./Restaurant/Reducer";
+import menuItemReducer from "./Menu/Reducer";
 
 const rootReducer=combineReducers({
     auth:authReducer,
-    restaurant:restaurantReducer
+    restaurant:restaurantReducer,
+    menu:menuItemReducer
 })
 
 export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
