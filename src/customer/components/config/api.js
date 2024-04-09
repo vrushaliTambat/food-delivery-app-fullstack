912d19ab2de40774c2f43131bba89e4c15542813
vrushaliTambat <vrushalitambat32@gmail.com>
2024-04-09 15:32:10 -0400

    changes in config api

--- src/customer/components/config/api.js
+++ src/customer/components/config/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const API_URL="http://localhost:5454";
+export const DEPLOYED_URL="foodrush-backend-production.up.railway.app"
 
 export const api = axios.create({
-    baseURL: API_URL, 
+    baseURL: DEPLOYED_URL, 
     headers: {
       'Content-Type': 'application/json',
     },
--- src/customer/pages/Profile/UserProfile.jsx
+++ src/customer/pages/Profile/UserProfile.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Button } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../State/Authentication/Action";
 
 const UserProfile = () => {
+  const { auth } = useSelector((store) => store);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleLogout = () => {
-    // navigate("/");
-    // dispatch(logout());
+    navigate("/");
+    dispatch(logout());
   };
   return (
     <div className="min-h-[80vh] flex flex-col justify-center items-center text-center">
       <div className="flex flex-col items-center justify-center">
         <AccountCircleIcon sx={{ fontSize: "9rem" }} />
-        <h1 className="py-5 text-2xl font-semibold">Vrushali</h1>
-        <p>Email : vrush@gmail.com</p>
+        <h1 className="py-5 text-2xl font-semibold">{auth.user?.fullName}</h1>
+        <p>Email : {auth.user?.email}</p>
         <Button
           onClick={handleLogout}
           variant="contained"
