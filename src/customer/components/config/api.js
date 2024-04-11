841ec26c9da94e111af0e226ed2c0ecfd27ae5bc
vrushaliTambat <vrushalitambat32@gmail.com>
2024-04-11 19:11:27 -0400

    config changes

--- src/customer/components/config/api.js
+++ src/customer/components/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const API_URL="http://localhost:5454";
-export const API_URL="https://foodrush-backend-production.up.railway.app";
+export const API_URL="http://localhost:5454";
+//export const API_URL="https://foodrush-backend-production.up.railway.app";
 
 export const api = axios.create({
     baseURL: API_URL, 
